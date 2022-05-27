2e4d4fe645d9f51b889c18344a0c0dbf3d706205
Suphasuta Prajamsri <suphprajam95@gmail.com>
2022-05-27 15:04:01 -0700

    restyle of watch face

--- src/components/Clock/index.jsx
+++ src/components/Clock/index.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import clsx from "clsx";
 import styles from "./styles.module.scss";
 
@@ -5,35 +6,49 @@ function Clock({ hours, minutes, seconds }) {
   const hoursDeg = (360 / 12) * (hours + minutes / 60 + seconds / 360);
   const minutesDeg = (360 / 60) * (minutes + seconds / 60);
   const secondsDeg = (360 / 60) * seconds;
+
+  // create 60 markings
+  const [markings, setMarkings] = useState([]);
+  useEffect(() => {
+    for (let i = 1; i <= 60; i++) {
+      setMarkings((curr) => {
+        return [
+          ...curr,
+          <div
+            className={styles.marking}
+            style={{
+              transform: `translate(-50%, 0)  rotate(${i * 6}deg) `,
+            }}
+            key={i}
+          ></div>,
+        ];
+      });
+    }
+  }, []);
   return (
-    <div className={styles.wrap}>
-      <div className={styles.outline}>
-        {/* <div>x</div> */}
-        <div>
-          <div className={styles.twelve}>12</div>
-          <div className={styles.three}>3</div>
-          <div className={styles.six}>6</div>
-          <div className={styles.nine}>9</div>
-        </div>
+    <div>
+      <div className={styles.clock}>
+        <div className={styles.center}></div>
+        <div>{markings}</div>
+        <div
+          className={clsx(styles.hour, styles.hand)}
+          style={{
+            transform: `translate(-50%, 0)  rotate(${hoursDeg}deg) translate(-50%, 0) `,
+          }}
+        ></div>
+        <div
+          className={clsx(styles.minute, styles.hand)}
+          style={{
+            transform: `translate(-50%, 0)  rotate(${minutesDeg}deg) translate(-50%, 0) `,
+          }}
+        ></div>
+        <div
+          className={clsx(styles.second, styles.hand)}
+          style={{
+            transform: `translate(-50%, 0)  rotate(${secondsDeg}deg)`,
+          }}
+        ></div>
       </div>
-      <div
-        className={clsx(styles.hour, styles.hand)}
-        style={{
-          transform: `translate(-50%, 0)  rotate(${hoursDeg}deg) translate(-50%, 0) `,
-        }}
-      ></div>
-      <div
-        className={clsx(styles.minute, styles.hand)}
-        style={{
-          transform: `translate(-50%, 0)  rotate(${minutesDeg}deg) translate(-50%, 0) `,
-        }}
-      ></div>
-      <div
-        className={clsx(styles.second, styles.hand)}
-        style={{
-          transform: `translate(-50%, 0)  rotate(${secondsDeg}deg) translate(-50%, 0) `,
-        }}
-      ></div>
     </div>
   );
 }
